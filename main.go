06eb03316f2f7738c93321e01dd4e2f2e3843474
Martin <m.t6kke@gmail.com>
2025-06-03 22:56:15 +0300

    c1 lesson 5 done

--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
+
 	server_mux := http.NewServeMux()
+	file_server := http.FileServer(http.Dir(filepathRoot))
+	server_mux.Handle("/", file_server)
 
 	server_struct := &http.Server{
 		Addr:    ":"+ port,
 		Handler: server_mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server_struct.ListenAndServe())
 }
