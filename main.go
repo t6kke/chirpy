0e73a9b7cd0a135b06fda150f8546765945e3048
Martin <m.t6kke@gmail.com>
2025-06-04 22:41:08 +0300

    progress on c1 lesson 11

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 
 	server_mux := http.NewServeMux()
 	file_server := http.FileServer(http.Dir(filepathRoot))
-	server_mux.Handle("/", file_server)
+	server_mux.Handle("/app/", http.StripPrefix("/app", file_server))
+	server_mux.HandleFunc("/healthz", healthzHandler)
 
 	server_struct := &http.Server{
 		Addr:    ":"+ port,
@@ -20,4 +21,11 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server_struct.ListenAndServe())
+}
+
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
