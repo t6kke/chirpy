3121c6975ce833ae49789ffa049258d97b78a848
Martin <m.t6kke@gmail.com>
2025-06-11 21:05:00 +0300

    c6 lesson 6 completed, added some tests to auth package

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,10 +1,18 @@
 package auth
 
 import (
-	"errors"
+	"fmt"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
+
+type MyCustomClaims struct {
+	Foo string `json:"foo"`
+	jwt.RegisteredClaims
+}
 
 func HashPassword(password string) (string, error) {
 	byte_pass_hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
@@ -17,7 +25,40 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(hash, password string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	if err != nil {
-		return errors.New("Passowrd not matching")
+		//return errors.New("Passowrd not matching")
+		return fmt.Errorf("Passowrd not matching %v", err)
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		Issuer:    "chirpy", //TODO issuer should be constant outside the function
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	result_string, err := token.SignedString([]byte(tokenSecret))
+	fmt.Println(result_string, err)
+
+	return result_string, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claimsStruct := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claimsStruct, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	//TODO validate issuer against the actual value
+	user_id, _ := token.Claims.GetSubject() //TODO handle error
+	return_uuid, _ := uuid.Parse(user_id) //TODO handle error
+
+	return return_uuid, nil
 }
